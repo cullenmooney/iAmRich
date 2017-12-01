040e849347c14b522762dc692594613912dcdba0
Cullen Mooney <cullenmooney@Cullens-MacBook.local>
2017-11-30 21:08:44 -0600

    Initial Commit

---
+++ I_Am_Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I_Am_Rich
+//
+//  Created by Cullen Mooney on 11/30/17.
+//  Copyright © 2017 Cullen Mooney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
